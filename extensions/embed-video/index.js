de20c3b13811642bd256507fb2938a8a0d15d42c
gaon12 <gokirito12@gmail.com>
2020-05-18 21:42:57 +0900

    니코동 추가

--- extensions/embed-video/index.js
+++ extensions/embed-video/index.js
@@ -1,36 +1,53 @@
 'use strict';
 
 function youtubeHook($item) {
-  const id = $item.text();
-  const width = Number($item.attr('width')) || 560;
-  const height = Number($item.attr('height')) || 315;
-  return `<iframe class="embed-video embed-video-youtube" src="//www.youtube-nocookie.com/embed/${id}" width="${width}" height="${height}"> </iframe>`;
+	  const id = $item.text();
+	  const width = Number($item.attr('width')) || 560;
+	  const height = Number($item.attr('height')) || 315;
+	  return `<iframe class="embed-video embed-video-youtube" src="//www.youtube-nocookie.com/embed/${id}" width="${width}" height="${height}" allowfullscreen> </iframe>`;
 }
 
 function kakaotvHook($item) {
-  const id = $item.text();
-  const width = Number($item.attr('width')) || 640;
-  const height = Number($item.attr('height')) || 360;
-  return `<iframe class="embed-video embed-video-kakaotv" width="${width}" height="${height}" src="//tv.kakao.com/embed/player/cliplink/${id}" allowfullscreen frameborder="0" scrolling="no"> </iframe>`;
+	  const id = $item.text();
+	  const width = Number($item.attr('width')) || 640;
+	  const height = Number($item.attr('height')) || 360;
+	  return `<iframe class="embed-video embed-video-kakaotv" width="${width}" height="${height}" src="//tv.kakao.com/embed/player/cliplink/${id}" allowfullscreen frameborder="0" scrolling="no"> </iframe>`;
+}
+
+function nicoHook($item) {
+	  const id = $item.text();
+	  const width = Number($item.attr('width')) || 640;
+	  const height = Number($item.attr('height')) || 360;
+	  return `<iframe class="embed-video embed-video-nico" width="${width}" height="${height}" src="//embed.nicovideo.jp/watch/${id}" allowfullscreen frameborder="0" scrolling="no"> </iframe>`;
 }
 
 module.exports.import = (helper) => {
-  helper.parserHook.setXmlHook({
-    type: 'afterSanitize',
-    selector: 'youtube',
-    hook: youtubeHook,
-    allowedTags: ['youtube'],
-    allowedAttributes: {
-      youtube: ['width', 'height'],
-    },
-  });
-  helper.parserHook.setXmlHook({
-    type: 'afterSanitize',
-    selector: 'kakaotv',
-    hook: kakaotvHook,
-    allowedTags: ['kakaotv'],
-    allowedAttributes: {
-      kakaotv: ['width', 'height'],
-    },
-  });
+	  helper.parserHook.setXmlHook({
+		      type: 'afterSanitize',
+		      selector: 'youtube',
+		      hook: youtubeHook,
+		      allowedTags: ['youtube'],
+		      allowedAttributes: {
+			            youtube: ['width', 'height'],
+			          },
+		    });
+	  helper.parserHook.setXmlHook({
+		      type: 'afterSanitize',
+		      selector: 'kakaotv',
+		      hook: kakaotvHook,
+		      allowedTags: ['kakaotv'],
+		      allowedAttributes: {
+			            kakaotv: ['width', 'height'],
+			          },
+		    });
+	helper.parserHook.setXmlHook({
+		    type: 'afterSanitize',
+		    selector: 'nico',
+		    hook: nicoHook,
+		    allowedTags: ['nico'],
+		    allowedAttributes: {
+			          kakaotv: ['width', 'height'],
+			        },
+		  });
 };
+
